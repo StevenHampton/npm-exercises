d098ca4d9714159c6d2fd5e7425f5675e1361ada
steven hampton <steven.hampton415@gmail.com>
2018-04-24 11:33:03 -0500

    added say-hello.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const $ = require('jquery');
+const greeting = require('say-hello');
 
 const sayHello = () => console.log("Hi, everybody!");
 
 sayHello();
+
+console.log(greeting);
+
 
 $("body").css("background-color", "blue");
 
